bd5346467c32f9a50292b1e984d670724b0db3a1
situx <situx@users.noreply.github.com>
2023-04-24 23:33:27 +0000

    Deploying to gh-pages from @ RGZM/ars-lod@78e7d2139e74ff293ba844f0b9c9b251b6839677 🚀

--- data/minivowl_result.js
+++
@@ -1,9 +0,0 @@
-var minivowlresult={
- "info": [
-  {
-   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
-  }
- ],
- "nodes": [],
- "links": []
-}
