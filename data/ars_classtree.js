8eb8fabf2c061ecdb8e414700a2a433854ce50d6
situx <situx@users.noreply.github.com>
2023-04-24 23:33:37 +0000

    Deploying to gh-pages from @ RGZM/ars-lod@78e7d2139e74ff293ba844f0b9c9b251b6839677 🚀

---
+++ data/ars_classtree.js
@@ -0,0 +1,46 @@
+var tree={
+  "plugins": [
+    "defaults",
+    "search",
+    "sort",
+    "state",
+    "types",
+    "contextmenu"
+  ],
+  "search": {
+    "show_only_matches": true
+  },
+  "types": {
+    "class": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/class.png"
+    },
+    "geoclass": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/geoclass.png"
+    },
+    "halfgeoclass": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/halfgeoclass.png"
+    },
+    "collectionclass": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/collectionclass.png"
+    },
+    "geocollection": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/geometrycollection.png"
+    },
+    "featurecollection": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/featurecollection.png"
+    },
+    "instance": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/instance.png"
+    },
+    "geoinstance": {
+      "icon": "https://cdn.jsdelivr.net/gh/i3mainz/geopubby@master/public/icons/geoinstance.png"
+    }
+  },
+  "core": {
+    "themes": {
+      "responsive": true
+    },
+    "check_callback": true,
+    "data": []
+  }
+}
